1ad5045a2c38a2f74d44ce9e895b4a3a344c722e
Ramk21 <57636169+Ramk21@users.noreply.github.com>
2020-09-04 23:34:43 +0530

    Add files via upload

---
+++ demo/src/main/java/com/example/ZipkinApp/ZipkinAppApplication.java
@@ -0,0 +1,16 @@
+package com.example.ZipkinApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import zipkin.server.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinAppApplication.class, args);
+	}
+
+}
